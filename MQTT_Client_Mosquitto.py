efd5a6d68cf53fced1d61e041e28db26b26457c6
UnmeshDeshpande <unmeshdeshpande.ud@gmail.com>
2022-05-01 19:01:51 +0200

    tweaks

--- MQTT_Client_Mosquitto.py
+++ MQTT_Client_Mosquitto.py
@@ -9,16 +9,24 @@ def on_message(client, userdata, message):
 
 #broker_address="10.2.167.173"
 broker_address="test.mosquitto.org"
-print("creating new instance")
+#print("creating new instance")
 client = mqtt.Client("UnmeshPC") #create new instance
 client.on_message=on_message #attach function to callback
-print("connecting to broker")
+#print("connecting to broker")
 client.connect(broker_address) #connect to broker
 client.loop_start() #start the loop
-print("Subscribing to topic","mosquitto/test1")
+#print("Subscribing to topic","mosquitto/test1")
+client.publish("mosquitto/test1","A")
 client.subscribe("mosquitto/test1")
-client.subscribe("mosquitto/test2")
-print("Publishing message to topic","mosquitto/test2")
+#print("Publishing message to topic","mosquitto/test2")
 client.publish("mosquitto/test2","OFF")
-time.sleep(4) # wait
+time.sleep(1) # wait
+client.publish("mosquitto/test1","B")
+time.sleep(1)
+client.publish("mosquitto/test1","C")
+time.sleep(1)
+client.publish("mosquitto/test2","ON")
+time.sleep(1)
+client.publish("mosquitto/test1","D")
+time.sleep(1)
 client.loop_stop() #stop the loop
